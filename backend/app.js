2eed78d88625f4ac3541b3ba898803a79c79f343
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-22 00:18:44 +0500

    Added CORS dependency and middleware

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static("uploads"));
 
