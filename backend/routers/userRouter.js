8852172e941039d255528e321b2c30625aea6e35
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-22 00:19:10 +0500

    Fixed the wrong multer file path

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const path = require("path");
-const { upload } = require("multer");
+const { upload } = require("../multer.js");
 const handleCreateUser = require("../controllers/user");
 
 router.post("/create-user", upload.single("file"), handleCreateUser);
