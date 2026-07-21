b57fd0ff091cde23eb1b944d7f33e7906728a80c
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-21 18:19:23 +0500

    Use Browser router,  RouterProvider and Outlet components from react-router hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router";
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <nav className="mb-3"></nav>
+      <main>
+        <Outlet />
+      </main>
+      <footer className="mt-3"></footer>
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { LoginPage } from "./pages";
+
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<LoginPage />} />
+    </Route>,
+  ),
+);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
