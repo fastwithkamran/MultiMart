7d395f9373c67821c27cef9b66baafad279549ee
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-22 21:00:16 +0500

    Added dependency toastify to display formal error messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <nav className="mb-3"></nav>
       <main>
         <Outlet />
