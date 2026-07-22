85b6a532c76c9713757b50760f383e56c061147e
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-22 23:04:35 +0500

    Implement isAuthenticated function and assign its route

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,9 +3,12 @@ const router = Router();
 const path = require("path");
 const { upload } = require("../multer.js");
 const { handleCreateUser, handleActivateUser, handleUserLogin } = require("../controllers/user");
+const { isAuthenticated } = require("../utils/auth.js");
 
 router.post("/create-user", upload.single("file"), handleCreateUser);
 router.post("/activation", handleActivateUser);
 router.post("/login", handleUserLogin);
+
+router.get("/getuser", isAuthenticated, );
 
 module.exports = router;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,6 +1,21 @@
-const ErrorHandler = require("./ErrorHandler.js")
-const catchAsyncErrors = require("./catchAsyncErrors.js")
-const jwt = require("jsonwebtoken")
+const ErrorHandler = require("./ErrorHandler.js");
+const catchAsyncErrors = require("./catchAsyncErrors.js");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
 
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
 
+  if (!token) {
+    return next(new ErrorHandler("Please Login to continue", 401));
+  }
 
+  const verifyUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  const user = await User.findById(verifyUser.id);
+
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
