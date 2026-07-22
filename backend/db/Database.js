612c1f3e81c17d910fe4234103cc90485eb42f52
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-22 16:27:37 +0500

    Fixed the critical syntax bugs; added cluster name in db; correct the params in catchAsyncErrors.js; fixed the file name auth.js

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017").then((data) => {
+  mongoose.connect("mongodb://127.0.0.1:27017/Multi_Vendor").then((data) => {
     console.log(`MongoDB connected ${data.connection.host}`);
   });
 };
