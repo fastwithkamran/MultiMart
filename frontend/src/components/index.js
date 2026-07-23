e5fa708d91af62f394b5378a86ecc452fe00ac3f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-23 21:53:44 +0500

    Create a common folder for Landing Page features

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,7 @@
 import Login from "./Login/Login.jsx";
 import Signup from "./Signup/Signup.jsx";
 import Header from "./Layout/Header.jsx";
-import Hero from "./Hero/Hero.jsx";
-import Categories from "./Categories/Categories.jsx";
+import Hero from "./Landing/Hero.jsx";
+import Categories from "./Landing/Categories.jsx";
 
 export { Login, Signup, Header, Hero, Categories };
