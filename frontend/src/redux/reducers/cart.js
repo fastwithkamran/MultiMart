ddce9d989cef89e6cb4cf1e1bf71c45d6adf2e3c
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-26 23:07:28 +0500

    Implement global cart state management with localStorage persistence

---
+++ frontend/src/redux/reducers/cart.js
@@ -0,0 +1,25 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+};
+
+export const cartReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase("addToCart", (state, action) => {
+      const item = action.payload;
+      const index = state.cart.findIndex((i) => i._id === item._id);
+
+      if (index !== -1) {
+        state.cart[index] = item;
+      } else {
+        state.cart.push(item);
+      }
+    })
+
+    .addCase("removeFromCart", (state, action) => {
+      state.cart = state.cart.filter((i) => i._id !== action.payload);
+    });
+});
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/user.js";
+import { cartReducer } from "./reducers/cart.js";
 
 const Store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
-  devTools: true, 
+  devTools: true,
 });
 
 export default Store;
