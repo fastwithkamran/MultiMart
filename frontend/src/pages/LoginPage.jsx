db6596195c6b7bd33ef4257a851d0072ca3d0ec9
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-26 15:10:52 +0500

    Add a conditional useEffect to disable login and signup pages once user is authenticated

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,20 @@
-import { Login } from "../components"
+import { useSelector } from "react-redux";
+import { Login } from "../components";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const {isAuthenticated} = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated === true) navigate("/");
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
       <Login />
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,14 @@
 import { Signup } from "../components";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function SignupPage() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated === true) navigate("/");
+  }, [isAuthenticated, navigate]);
   return (
     <div>
       <Signup />
