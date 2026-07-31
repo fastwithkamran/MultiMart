b36a4549ff96f7b5f2857a5e0d9e6f03fc608f98
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-31 23:19:50 +0500

    Complete the handleSubmit function, also use redux for state management and react useEffect hook

--- frontend/src/components/Shop/Layout/Main/ShopCreateProduct.jsx
+++ frontend/src/components/Shop/Layout/Main/ShopCreateProduct.jsx
@@ -1,37 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { categoriesData } from "../../../../static/data";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import createProduct from "../../../../redux/actions/product";
+import { resetSuccess, clearErrors } from "../../../../redux/reducers/product";
+import { toast } from "react-toastify";
 
 const ShopCreateProduct = () => {
   const { seller } = useSelector((state) => state.seller);
+  const { success, error } = useSelector((state) => state.product);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [images, setImages] = useState([]);
   const [name, setName] = useState("");
-  const [description, setDiscription] = useState("");
+  const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
-  const [tags, setTags] = useState();
+  const [tags, setTags] = useState("");
   const [originalPrice, setOriginalPrice] = useState();
   const [discountPrice, setDiscountPrice] = useState();
   const [stock, setStock] = useState();
 
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearErrors());
+    }
+    if (success) {
+      toast.success("Product Created Successfully");
+      dispatch(resetSuccess());
+      navigate("/dashboard");
+    }
+  }, [error, success, navigate, dispatch]);
+
   const handleSubmit = (e) => {
-    e.preventDefault;
+    e.preventDefault();
+    const newForm = new FormData();
+
+    images.forEach((image) => {
+      newForm.append("images", image);
+    });
+
+    newForm.append("name", name);
+    newForm.append("description", description);
+    newForm.append("category", category);
+    newForm.append("tags", tags);
+    newForm.append("originalPrice", originalPrice);
+    newForm.append("discountPrice", discountPrice);
+    newForm.append("stock", stock);
+    newForm.append("shopId", seller._id);
+
+    dispatch(createProduct(newForm));
   };
 
   const handleImageChange = (e) => {
     e.preventDefault;
-    console.log(e.target.files);
     let files = Array.from(e.target.files);
     setImages((prev) => [...prev, ...files]);
   };
   return (
-    <div className="w-[90%] md:w-[50%] bg-white shadow h-[80vh] rounded-sm p-3 overflow-y-auto">
+    <div className="w-[90%] md:w-[75%] bg-white shadow h-[80vh] rounded-sm p-3 overflow-y-auto">
       <h5 className="text-[30px] font-Poppins text-center">Create Product</h5>
       {/* Create Product Form */}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <br />
         <div>
           <label className="pb-2">
@@ -53,14 +84,16 @@ const ShopCreateProduct = () => {
           <label className="pb-2">
             Description <span className="text-red-500">*</span>
           </label>
-          <input
-            className="bg-slate-200 appearance-none mt-2 block w-full h-7 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm px-3"
-            type="text"
+          <textarea
+            cols="30"
+            rows="8"
+            required
+            className="bg-slate-200 pt-3 appearance-none mt-2  w-full border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm px-3"
             placeholder="Enter product description..."
             name="description"
             value={description}
-            onChange={(e) => setDiscription(e.target.value)}
-          />
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
         </div>
 
         {/* Category */}
@@ -162,10 +195,10 @@ const ShopCreateProduct = () => {
               <AiOutlinePlusCircle size={30} className="mt-3" color="#555" />
             </label>
             {images &&
-              images.map((i) => (
+              images.map((i, index) => (
                 <img
                   src={URL.createObjectURL(i)}
-                  key={i}
+                  key={index}
                   alt="ProductImage"
                   className="h-24 w-24 object-cover m-2"
                 />
@@ -173,7 +206,11 @@ const ShopCreateProduct = () => {
           </div>
           <br />
           <div>
-            <input type="submit" value={"Create"} className="mt-2 cursor-pointer appearance-none text-center block w-full h-8 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm px-3"/>
+            <input
+              type="submit"
+              value={"Create"}
+              className="mt-2 cursor-pointer appearance-none text-center block w-full h-8 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm px-3"
+            />
           </div>
         </div>
       </form>
