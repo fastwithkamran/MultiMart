b5a48116418119400e916355afc10610d929959f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-02 00:02:39 +0500

    Built the apis to Get All Events and to Delete a event

--- backend/controllers/event.js
+++ backend/controllers/event.js
@@ -31,4 +31,36 @@ const handleCreateEvent = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
-module.exports = handleCreateEvent;
+// get all events of a shop
+const handleGetShopEvents = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const events = await Event.find({ shopId: req.params.id });
+    res.status(201).json({
+      success: true,
+      events,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error, 500));
+  }
+});
+
+// delete event
+const handleDeleteEvent = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const eventId = req.params.id;
+    const event = await Event.findByIdAndDelete(eventId);
+
+    if (!event) {
+      return next(new ErrorHandler("Event not found with this id", 400));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Event Deleted Successfully",
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error, 500));
+  }
+});
+
+module.exports = { handleCreateEvent, handleDeleteEvent, handleGetShopEvents };
--- backend/routers/eventRouter.js
+++ backend/routers/eventRouter.js
@@ -5,8 +5,20 @@ const { upload } = require("../multer");
 const { isAuthenticatedSeller } = require("../utils/auth");
 const catchAsyncErrors = require("../utils/catchAsyncErrors");
 
-const handleCreateEvent = require("../controllers/event");
+const {
+  handleCreateEvent,
+  handleGetShopEvents,
+  handleDeleteEvent,
+} = require("../controllers/event");
 
 router.post("/create-event", upload.array("images"), handleCreateEvent);
+
+router.get("/get-all-events/:id", handleGetShopEvents);
+
+router.delete(
+  "/delete-shop-event/:id",
+  isAuthenticatedSeller,
+  handleDeleteEvent,
+);
 
 module.exports = router;
