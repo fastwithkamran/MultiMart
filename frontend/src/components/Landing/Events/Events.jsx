d0652947c7c5da0523140cc124a6cfbcbea0160d
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-03 20:52:08 +0500

    Optimized the Event Section and Card with dynamic data

--- frontend/src/components/Landing/Events/Events.jsx
+++ frontend/src/components/Landing/Events/Events.jsx
@@ -1,17 +1,25 @@
 import styles from "../../../styles/styles";
 import EventCard from "./EventCard";
+import { useSelector } from "react-redux";
+import Loader from "../../Layout/Loader/Loader";
 
 function Events() {
+  const { allEvents, isLoading } = useSelector((state) => state.event);
   return (
-    <div className="w-full">
-      <div className={`${styles.heading} ml-5`}>
-        <h1>Popular Events</h1>
-      </div>
-
-      <div className="w-full grid">
-        <EventCard />
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : allEvents.length !== 0 ? (
+        <div className="w-full">
+          <div className={`${styles.heading} ml-5`}>
+            <h1>Popular Events</h1>
+          </div>
+          <div className="w-full grid">
+            <EventCard data={allEvents && allEvents[0]} />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
 
--- frontend/src/pages/LandingPages/EventsPage.jsx
+++ frontend/src/pages/LandingPages/EventsPage.jsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
 import { Header, EventCard } from "../../components";
+import { Loader } from "../../components";
 
 function EventsPage() {
+  const { allEvents, isLoading } = useSelector((state) => state.event);
+
   return (
-    <div>
-      <Header activePage={4} />
-      <div className="p-5">
-        <EventCard active={true} />
-        <EventCard active={true} />
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header activePage={4} />
+          <div className="p-5">
+            <EventCard active={true} data={allEvents && allEvents[0]} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
