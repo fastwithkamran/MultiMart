00d3df0c02e48038c42353c21e25161617a56ea0
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-10 21:40:20 +0500

    Add package.json for socket.io package with dependencies and scripts to manage a real-time chat application

---
+++ socket/index.js
@@ -0,0 +1,126 @@
+const socketIO = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./.env",
+});
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Socket IO Connected Successfully!");
+});
+
+// maintain an array of active users
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+// when user left chat, this function will remove user from active users array
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+// find a specfic online user by its db id
+const getUser = (receiverId) => {
+  return users.find((user) => user.userId === receiverId);
+};
+
+// Define message object data
+const createMessage = ({ senderId, receiverId, text, images }) => ({
+  senderId,
+  receiverId,
+  text,
+  images,
+  seen: false,
+});
+
+// manage active websocket connections
+io.on("connection", (socket) => {
+  // when connected
+  console.log("User is connected!");
+
+  // link userId to their active socketId
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    // broadcasts everyone who is connected
+    io.emit("getUsers", users);
+  });
+
+  // send and get message
+  const messages = {}; // Object to track messages for each user
+
+  socket.on("sendMessage", ({ senderId, receiverId, text, images }) => {
+    const message = createMessage({ senderId, receiverId, text, images });
+    const user = getUser(receiverId);
+
+    // Store the messages in the 'message' object
+    if (!messages[receiverId]) {
+      messages[receiverId] = [message];
+    } else {
+      messages[receiverId].push(message);
+    }
+
+    // send the message to the receiver
+    io.to(user?.socketId).emit("getMessage", message);
+  });
+
+  socket.on("messageSeen", ({ senderId, receiverId, messageId }) => {
+    const user = getUser(senderId);
+
+    // update the seen flag for the message
+    if (messages[senderId]) {
+      const message = messages[senderId].find(
+        (message) =>
+          message.receiverId === receiverId && message.id === messageId,
+      );
+      if (message) {
+        message.seen = true;
+      }
+
+      // send a message seen event to the sender
+      io.to(user?.socketId).emit("messageSeen", {
+        senderId,
+        receiverId,
+        messageId,
+      });
+    }
+  });
+
+  // update and get new message
+  socket.on("updateNewMessage", ({ newMessage, newMessageId }) => {
+    io.emit("getNewMessage", {
+      newMessage,
+      newMessageId,
+    });
+  });
+
+  // disconnect users from socket
+  socket.on("disconnect", () => {
+    console.log("User disconnected from socket");
+
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Server started at PORT", process.env.PORT);
+});
