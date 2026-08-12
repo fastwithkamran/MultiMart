0b2facb503e66de01fd0c1520bc3f5310569fead
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-12 22:25:53 +0500

    Add loading state in ActivationPages and design the pages to return proper message with link to login button if the token has expired

--- frontend/src/pages/SellerAuthPages/SellerActivationPage.jsx
+++ frontend/src/pages/SellerAuthPages/SellerActivationPage.jsx
@@ -4,14 +4,20 @@ import { useParams } from "react-router-dom";
 import { server } from "../../../server";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Loader } from "../../components";
+import { Link } from "react-router-dom";
+import styles from "../../styles/styles";
 
 function SellerActivationPage() {
   const { activation_token } = useParams();
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (activation_token) {
       const activationEmail = async () => {
+        setLoading(true);
         try {
           const res = await axios.post(
             `${server}/shop/activation`,
@@ -27,13 +33,31 @@ function SellerActivationPage() {
           } else {
             toast.error(res.data.message);
           }
+          setLoading(false);
         } catch (error) {
+          setLoading(false);
+          toast.error(error.response.data.message);
           console.error(error.response.data.message);
         }
       };
       activationEmail();
     }
   }, [activation_token, navigate]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col w-full h-screen items-center justify-center text-2xl text-red-400">
+          Your Email Verification Code Has Expired. Try again!
+          <Link to={"/login"}>
+            <div className={`${styles.button} text-white p-3`}>Go To Login</div>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default SellerActivationPage;
--- frontend/src/pages/UserAuthPages/ActivationPage.jsx
+++ frontend/src/pages/UserAuthPages/ActivationPage.jsx
@@ -1,20 +1,25 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { server } from "../../../server";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loadUser } from "../../redux/actions/user";
+import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
+import { Loader } from "../../components";
 
 function ActivationPage() {
   const { activation_token } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (activation_token) {
       const activationEmail = async () => {
+        setLoading(true);
         try {
           const res = await axios.post(
             `${server}/user/activation`,
@@ -31,13 +36,31 @@ function ActivationPage() {
           } else {
             toast.error(res.data.message);
           }
+          setLoading(false);
         } catch (error) {
+          setLoading(false);
+          toast.error(error.response.data.message);
           console.error(error.response.data.message);
         }
       };
       activationEmail();
     }
   }, [activation_token, navigate, dispatch]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col w-full h-screen items-center justify-center text-2xl text-red-400">
+          Your Email Verification Code Has Expired. Try again!
+          <Link to={"/login"}>
+            <div className={`${styles.button} text-white p-3`}>Go To Login</div>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ActivationPage;
