5a713931c61e42b03c93cd305b9fd66efe2c6dfd
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-12 13:06:02 +0500

    Update database connection logic for deployment to handle connection states

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,9 +1,33 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/Multi_Vendor").then((data) => {
-    console.log(`MongoDB connected ${data.connection.host}`);
-  });
+const connectDatabase = async () => {
+  // already connected
+  if (mongoose.connection.readyState === 1) {
+    return;
+  }
+
+  // waiting for connection if succcess then resolve else reject
+  if (mongoose.connection.readyState === 2) {
+    await new Promise((resolve, reject) => {
+      mongoose.connection.once("connected", resolve);
+      mongoose.connection.once("error", reject);
+    });
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      // if db not connected throw error immediately instead of waiting 10 sec
+      bufferCommands: false,
+      // wait atmost 5 sec to find a mongodb server
+      serverSelectionTimeoutMS: 5000,
+      // wait maximum 10 sec for query response
+      socketTimeoutMS: 10000,
+    });
+  } catch (err) {
+    console.error("MongoDB Connection failed", err.message);
+    throw err;
+  }
 };
 
 module.exports = connectDatabase;
