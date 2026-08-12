8918d2a88932c5ead981fceea1a7b03cb041a566
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-08-12 12:40:31 +0500

    Update image upload handling to use multer memory storage and remove multer-storage-cloudinary dependency

--- backend/multer.js
+++ backend/multer.js
@@ -1,30 +1,63 @@
-const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 const path = require("path");
+const multer = require("multer");
+const { Readable } = require("stream");
 
-const cloudinaryModule = require("cloudinary");
-const cloudinary = cloudinaryModule.v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
+// pass env credentials
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "multi-vendor/images",
+// save files in RAM as a buffer
+const storage = multer.memoryStorage();
+// use in routes multer put the images file in req.file.buffer
+exports.upload = multer({ storage });
 
-    public_id: (req, file) => {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      const ext = path.extname(file.originalname);
-      const filename = path.basename(file.originalname, ext);
+// upload to cloudinary
+exports.uploadToCloudinary = (fileBuffer, originalname) => {
+  return new Promise((resolve, reject) => {
+    const ext = path.extname(file.originalname);
+    const filename = path.basename(file.originalname, ext);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const public_id = `multi-vendor/images/${filename}-${uniqueSuffix}`;
 
-      return filename + "-" + uniqueSuffix;
-    },
-  },
-});
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        public_id,
+        transformation: [
+          {
+            fetch_format: "auto",
+            quality: "auto",
+          },
+          {
+            width: 1200,
+            height: 1200,
+            crop: "fill",
+            gravity: "auto",
+          },
+        ],
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      },
+    );
 
-exports.upload = multer({ storage: storage });
-exports.cloudinary = cloudinary;
+    // convert the buffer to stream
+    const readable = new Readable();
+    readable.push(fileBuffer);
+    readable.push(null);
+    readable.pipe(uploadStream);
+  });
+};
+
+// delete from cloudinary
+exports.deleteFromCloudinary = async (public_id) => {
+  try {
+    await cloudinary.uploader.destroy(public_id);
+  } catch (error) {
+    console.error("Cloudinary delete error", error);
+  }
+};
